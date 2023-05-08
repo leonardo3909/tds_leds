875c602df5c8292fdc8f2e798a5fdcadbf340ab8
leonardo3909 <127439867+leonardo3909@users.noreply.github.com>
2023-05-08 17:48:28 -0500

    leds

---
+++ src/main/java/co/edu/umanizales/lde_leds/LdeLedsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.lde_leds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdeLedsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdeLedsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/umanizales/lde_leds/LdeLedsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.lde_leds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdeLedsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
